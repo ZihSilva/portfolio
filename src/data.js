b2a2e7d403143aa98a242edfdc8ab295f10b8aa4
zisilva <zimarlensilva@gmail.com>
2023-01-04 22:07:26 -0800

    fixing project page

--- src/data.js
+++ src/data.js
@@ -36,7 +36,7 @@ const data = [
     {
       id: 4,
       img: imagesData.DanceSchool,
-      title: "Leap Forward Dance School",
+      title: "LF Dance School",
       description:
         "This is a landing page for a dance school that is built using React, JavaScript, and Tailwind CSS. The page is designed to be compatible with all mobile devices and features a user-friendly interface. The page is intended to provide information about the dance school.",
       tools: ["React", "Javascript", "Tailwind CSS"],
